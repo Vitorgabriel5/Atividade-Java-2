f320348c8d0bb4393e315d44c1e9f4d5b8f2956b
vitor gabriel da silva pereira <gabrielsorriso666@gmail.com>
2024-12-27 21:14:58 -0300

    Atividade 2 completa

---
+++ src/aplication/Main.java
@@ -0,0 +1,34 @@
+package aplication;
+
+import objet.Employee;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee emp = new Employee();
+
+        System.out.print("Name: ");
+        emp.name = sc.nextLine();
+        System.out.print("Gross Salary: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        emp.tax = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Employees: " + emp);
+        System.out.println();
+        System.out.print("Which Percentage to increase Salary? ");
+        double percentage = sc.nextDouble();
+       emp.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.println("Update data " + emp);
+
+        sc.close();
+    }
+}
